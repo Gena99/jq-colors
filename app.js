3e3e325c35184d53f3d655c046c7c7fdd96f1ff7
Gena <gena@e64.net>
2017-01-07 16:51:08 +0100

    ajout d'un gitignore et ajout des couleurs sur les boutons

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // good luck !
+$('document').ready(function(){
+	//$("[type='button']").css( "background-color", "red" );
+	
+	// Creer un objet qui stoke la liste de tous les boutons
+	var boutons = $("[type='button']");
+	
+	// Parcourir la liste des boutons et attribuer à chacun sa couleur
+	for (var i = boutons.length - 1; i >= 0; i--) {
+		//boutons[i]
+		//$(boutons[i]).css('background-color',$(boutons[i]).attr('data-color'));
+		//Creer une variable qui contient mon bouton et une pour la couleur
+		var monBouton = $(boutons[i]);
+		var couleur = monBouton.attr('data-color');
+		monBouton.css('background-color', couleur);
+	}
+
+});
